ef11f0320e5f5290ebc529cebc7fabfcadc0e4a4
hulyak <57311842+hulya95@users.noreply.github.com>
2020-09-08 15:41:03 -0700

    complete the products page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,16 +13,61 @@ function Home() {
           alt=""
         />
         <div className="home__row">
-          <Product />
-        </div>
-
-        {/* <div className="home__row">
-
+          <Product
+            title="Echo Dot with The Child stand"
+            price="79.99"
+            image="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/M2RjMzI2ZGUt/M2RjMzI2ZGUt-NjkwMjZhZDEt-w758._SY608_CB406624432_.jpg"
+            rating={5}
+          />
+          <Product
+            title="Go wild with trendy prints"
+            price="29.00"
+            image="https://images-na.ssl-images-amazon.com/images/G/01/PLF/LARKRO/2020/SPRING-DRIVERS/LR-PRINT-TOP_DT_CC758x608-2X._SY608_CB410269718_.jpg"
+            rating={5}
+          />
         </div>
 
         <div className="home__row">
+          <Product
+            title="The new Halo wellness band"
+            price="64.99"
+            image="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/YjJkZWQ0NTgt/YjJkZWQ0NTgt-MjhiNzdlMmIt-w758._SY608_CB406527709_.jpg"
+            rating={5}
+          />
+          <Product
+            title="Deals on apparel in Outlet"
+            price="9.99"
+            image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/Outlet/Merchandising/Outlet_GW_Coupons_DSC_758x608._SY608_CB429449198_.jpg"
+            rating={5}
+          />
+          <Product
+            title="Shop medical supplies"
+            price="49.99"
+            image="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Health_x26_Personal_Care/XCM_Manual_1239353_1269289_US_us_health_personal_care_consumables_desktop_cat_card_hpc_1237578_2_3240978_758x608_2X_en_US._SY608_CB408425348_.jpg"
+            rating={5}
+          />
+          <Product
+            title="Deals on overstock shoes"
+            price="14.99"
+            image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/Outlet/Merchandising/Outlet_GW_SH_758x608_B07Z4CP5K1_920fe539._SY608_CB406458921_.jpg"
+            rating={5}
+          />
+        </div>
 
-        </div> */}
+        <div className="home__row">
+          <Product
+            title="Gabrielle Union's Flawless Collection"
+            price="9.99"
+            image="https://images-na.ssl-images-amazon.com/images/I/61QPh3J65iL._AC_SY400_.jpg"
+            rating={5}
+          />
+          <Product
+            title="Protect your home with Flo by Moen"
+            price="49.99"
+            image="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/NDQxZWMzNmQt/NDQxZWMzNmQt-NTg5MjhlYWUt-w758._SY608_CB407437045_.jpg"
+            rating={5}
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import './Product.css';
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>Echo Dot with The Child stand</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>79.99</strong>
+          <strong>{price}</strong>
         </p>
 
         <div className="product__rating">
-          <p>⭐️</p>
-          {/* <p>⭐️</p>
-          <p>⭐️</p>
-          <p>⭐️</p>
-          <p>⭐️</p> */}
+          {/* dynamically change the rating stars */}
+          {Array(rating).fill().map((_, i) => <p>⭐️</p>)}
         </div>
       </div>
 
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/M2RjMzI2ZGUt/M2RjMzI2ZGUt-NjkwMjZhZDEt-w758._SY608_CB406624432_.jpg"
-        alt=""
-      />
+      <img src={image} alt="" />
 
       <button>Add to Basket</button>
     </div>
