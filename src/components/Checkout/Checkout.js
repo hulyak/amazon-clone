c665ca637ed292d591d58f59fbc154a7581bea77
hulyak <57311842+hulya95@users.noreply.github.com>
2020-09-09 16:56:10 -0700

    change icon

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -21,13 +21,14 @@ function Checkout() {
           <h2 className="checkout__title">Your Shopping Basket</h2>
           {basket.map((item) => (
             <CheckoutProduct
+              key={item.id}
               id={item.id}
               title={item.title}
               image={item.image}
               price={item.price}
               rating={item.rating}
             />
-          ))}
+           ))}
         </div>
       </div>
 
--- src/components/CheckoutProduct/CheckoutProduct.js
+++ src/components/CheckoutProduct/CheckoutProduct.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './CheckoutProduct.css';
 import { useStateValue } from '../../StateProvider';
+import FlipMove from 'react-flip-move';
 
 function CheckoutProduct({ id, image, title, price, rating }) {
   const [{ basket }, dispatch] = useStateValue();
